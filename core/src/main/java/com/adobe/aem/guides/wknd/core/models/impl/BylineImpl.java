c5e42709502e7330e2e62624c9a5bd28b8a80750
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-01-28 18:48:16 +0100

    Create Byline SLing model (with image)

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImpl.java
@@ -3,14 +3,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import com.adobe.aem.guides.wknd.core.models.Byline;
+import com.adobe.cq.wcm.core.components.models.Image;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-
+import org.apache.sling.models.factory.ModelFactory;
 
 @Model(
         adaptables = {SlingHttpServletRequest.class},
@@ -22,12 +27,25 @@ public class BylineImpl implements Byline {
 
   protected static final String RESOURCE_TYPE = "wknd/components/content/byline";
 
+  @Self
+  private SlingHttpServletRequest request;
+
+  @OSGiService
+  private ModelFactory modelFactory;
+
   @ValueMapValue
   private String name;
 
   @ValueMapValue
   private List<String> occupations;
 
+  private Image image;
+
+  
+  @PostConstruct
+  private void init() {
+      image = modelFactory.getModelFromWrappedRequest(request, request.getResource(), Image.class);
+  }
 
   @Override
   public String getName() {
@@ -46,20 +64,27 @@ public class BylineImpl implements Byline {
 
   @Override
   public boolean isEmpty() {
-    
+    final Image componentImage = getImage();
     if ( StringUtils.isBlank(name) ) {
       // Name is missing, but required
       return true;
     } else if ( occupations == null || occupations.isEmpty() ) {
       // At least one occupation is required
       return true;
-    } else if ( false ) {
+    } else if ( componentImage == null || StringUtils.isBlank(componentImage.getSrc()) ) {
       // A valid image is required
       return true;
     } else {
       // Everything is populated, so this component is not considered empty
       return false;
     }
+  }
+
+  /**
+  * @return the Image Sling Model of this resource, or null if the resource cannot create a valid Image Sling Model.
+  */
+  private Image getImage() {
+    return image;
   }
 
 }
