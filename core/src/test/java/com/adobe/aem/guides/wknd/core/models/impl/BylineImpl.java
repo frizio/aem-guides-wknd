45ad7fe013bfb67a11a09a4f32ed1f8d7b8ded9b
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-02 19:31:12 +0100

    Scheletro BylineImpl test class

--- core/src/test/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImpl.java
+++
@@ -1,5 +0,0 @@
-package com.adobe.aem.guides.wknd.core.models.impl;
-
-public class BylineImpl {
-  
-}
--- core/src/test/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImplTest.java
+++ core/src/test/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImplTest.java
@@ -1,5 +1,29 @@
 package com.adobe.aem.guides.wknd.core.models.impl;
 
-public class BylineImpl {
-  
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BylineImplTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+    }
+
+    @Test 
+    void testGetName() { 
+        fail("Not yet implemented");
+    }
+    
+    @Test 
+    void testGetOccupations() { 
+        fail("Not yet implemented");
+    }
+
+    @Test 
+    void testIsEmpty() { 
+        fail("Not yet implemented");
+    }
 }
