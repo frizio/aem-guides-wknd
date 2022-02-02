7ae514752667a83c8e748d30b30b1f3dffc8b4e9
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-02 19:40:14 +0100

    BylineImplTest - part 1

--- core/src/test/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImplTest.java
+++ core/src/test/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImplTest.java
@@ -2,20 +2,37 @@ package com.adobe.aem.guides.wknd.core.models.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.adobe.aem.guides.wknd.core.models.Byline;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+
+@ExtendWith(AemContextExtension.class)
 public class BylineImplTest {
+
+  private final AemContext ctx = new AemContext();
 
     @BeforeEach
     void setUp() throws Exception {
-
+      ctx.addModelsForClasses(BylineImpl.class);
+      ctx.load().json("/com/adobe/aem/guides/wknd/core/models/impl/BylineImplTest.json", "/content");
     }
 
     @Test 
-    void testGetName() { 
-        fail("Not yet implemented");
-    }
+    public void testGetName() {
+      final String expected = "Jane Doe";
+  
+      ctx.currentResource("/content/byline");
+      Byline byline = ctx.request().adaptTo(Byline.class);
+  
+      String actual = byline.getName();
+  
+      assertEquals(expected, actual);
+  }
     
     @Test 
     void testGetOccupations() { 
